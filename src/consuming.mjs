76a3fbcc8aeee1235767197fd6c4996ebb2e02df
sam-klok <sklok11@gmail.com>
2021-09-16 11:20:13 -0700

    adding promise chain implementation

--- src/consuming.mjs
+++ src/consuming.mjs
@@ -1,12 +1,38 @@
 import setText, {appendText, showWaiting, hideWaiting} from "./results.mjs";
 
 export function get() {
+    axios.get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+        setText(JSON.stringify(data));
+    })
 }
 
 export function getCatch() {
+    // axios.get("http://localhost:3000/orders/123")
+    // .then((result) => {
+    //     if (result.status === 200){
+    //         setText(JSON.stringify(result.data))
+    //     } else{
+    //         setText('Error');
+    //     };
+    //})
+
+    axios.get("http://localhost:3000/orders/123")
+    .then(({data}) => {
+        setText(JSON.stringify(data));
+    })
+    .catch(err => setText(err));
+
 }
 
 export function chain() {
+    axios.get("http://localhost:3000/orders/1")
+    .then(({data}) => {
+        axios.get(`http://localhost:3000/addresses/${data.shippingAddress}`);
+    })
+    .then(({data})=>{
+        setText(`City: ${data.city}`);
+    });
 }
 
 export function chainCatch() {
